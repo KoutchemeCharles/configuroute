37f0943fde3a2c8e51245255d4f8143a0a17a8fa
Charles Koutcheme <charles.koutcheme@gmail.com>
2019-07-18 16:05:18 +0200

    Updates

---
+++ precache-manifest.317d66518c2ace11ae49d9951d0573f9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1a7ac9e404fc3324be4712a672a598d0",
+    "url": "/configuroute/index.html"
+  },
+  {
+    "revision": "5a10bc4dcecbd45bffe9",
+    "url": "/configuroute/static/css/2.6639d161.chunk.css"
+  },
+  {
+    "revision": "0a79947fa5ab76172aae",
+    "url": "/configuroute/static/css/main.957d602a.chunk.css"
+  },
+  {
+    "revision": "5a10bc4dcecbd45bffe9",
+    "url": "/configuroute/static/js/2.cfe41e0f.chunk.js"
+  },
+  {
+    "revision": "0a79947fa5ab76172aae",
+    "url": "/configuroute/static/js/main.55573ded.chunk.js"
+  },
+  {
+    "revision": "e1d580cf93e431b2567c",
+    "url": "/configuroute/static/js/runtime~main.250062d1.js"
+  },
+  {
+    "revision": "47bb72e506eba473225b7437ac7bdebf",
+    "url": "/configuroute/static/media/logo.47bb72e5.jpg"
+  }
+]);
